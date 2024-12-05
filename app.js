e0fe84c72bbf741f8754c71ac03011c65b61bcb3
praveenkumar9538 <167498924+praveenkumar9538@users.noreply.github.com>
2024-12-05 15:30:39 +0530

    Update app.js
    updated to express

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, Jenkins updated!\n');
+// Simple route for testing
+app.get('/', (req, res) => {
+    res.send('Hello, Jenkins updated!');
 });
 
-server.listen(3000, () => {
-    console.log('Server running at http://localhost:3000/');
+// Listening on all interfaces (important for GCP)
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running at http://0.0.0.0:${port}`);
 });
-
